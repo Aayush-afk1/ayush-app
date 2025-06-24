512a5ba899ec05f2bad3f739e800ab1ab5fec877
Aayush-afk1 <14ryayush9@gmail.com>
2025-06-24 10:28:14 +0545

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,42 +1,20 @@
-const CACHE_NAME = "baby-cache-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon.png"
-];
 
-self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Install");
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("[Service Worker] Caching all: app shell and content");
-      return cache.addAll(FILES_TO_CACHE);
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('aakriti-store').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './icon.png'
+      ]);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("[Service Worker] Activate");
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
