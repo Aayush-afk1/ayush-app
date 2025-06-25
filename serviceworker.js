bf485dedf80df80e354d2d351cc2f7d4162cb8e8
Aayush-afk1 <14ryayush9@gmail.com>
2025-06-25 08:47:36 +0545

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,20 +1,33 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('app-cache').then(function(cache) {
+const CACHE_NAME = 'app-cache-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
         '/icon.png',
+        '/icon1.png',
         '/manifest.json'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
