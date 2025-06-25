17a36009a60fdbbed9e7e71a97d7a0fd7a18722e
Aayush-afk1 <14ryayush9@gmail.com>
2025-06-25 08:29:32 +0545

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,51 +1,20 @@
-const CACHE_NAME = 'hello-aakriti-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png',
-  './icon1.png',
-  // Add other assets like CSS, JS if needed
-];
-
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('aakriti-store').then(function(cache) {
+    caches.open('app-cache').then(function(cache) {
       return cache.addAll([
-        './',
-        './index.html',
-        './icon.png'
+        '/',
+        '/index.html',
+        '/icon.png',
+        '/manifest.json'
       ]);
     })
-// Install event - cache app shell
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
   );
-});
-
-// Activate event - cleanup old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key)))
-    )
-);
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
-// Fetch event - serve cached content or fetch network
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-})
-);
+    })
+  );
 });
